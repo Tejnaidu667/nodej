80c88ef97dd53016334f41e7dc9675a74ca515f7
tejnaidu667 <moturitejaganesh@gmail.com>
2026-09-14 16:29:20 +0530

    Added callback and call stack examples

---
+++ script.js
@@ -0,0 +1,62 @@
+//the first function
+
+// function dosomething(name) 
+// {
+//     console.log("hello"+ name);
+// }
+
+// dosomething("John");
+// dosomething("Doe");
+
+//the second function
+
+// function dosomething(n) 
+// {
+//     console.log(n);
+// }
+
+// dosomething(demo);
+
+// function demo()
+// {
+//     console.log("chutiya");
+// }
+
+// the third function
+
+// function dosomething(n) 
+// {
+//     n();
+// }
+
+// dosomething(demo);
+
+// function demo()
+// {
+//     console.log("chutiya");
+// }
+
+//instand of third we can use 
+
+// function dosomething(n) 
+// {
+//     n();
+// }
+
+// dosomething(function(){
+//     console.log("chutiya");
+// });
+
+// giving data and getting data back using callback function
+
+// function getuserdata(callback) {
+
+//     let data = [1, 2, 3, 4, 9, 5, 6, 7, 8, 3];
+
+//     callback(data);
+// }
+
+// getuserdata(function(userdata) {
+//     console.log(userdata);
+// });
+
